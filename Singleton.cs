20a2f705ddde06b20ba180379fd63cc593d9953a
Hunter Dyar <bloop@hdyar.com>
2021-05-13 12:52:07 -0400

    singleton clarifications

--- Singleton.cs
+++ Singleton.cs
@@ -2,11 +2,14 @@ using System;
 using UnityEngine;
 
 /// <summary>
-/// Usage is "public class GameManagerTHing : Singleton<GameManagerThing>" and then just reference Instance.
-/// It will create itself (lazy load on first get), you dont have to add it to the scene.
+/// Usage is "public class GameManagerThing : Singleton<GameManagerThing>" and then just reference Instance.
+/// It will create itself (lazy load on first get, as an otherwise empty object in DontDestroyOnLoad scene), you dont have to add it to the scene.
 /// But you can if you want.
 ///
-/// specific implementation is from Hands-On Game Development Patterns with Unity 2019 by David Baron.
+/// Do you actually want to use a singleton for this? Or is this going to add race conditions! Try to have singletons not need initialization from other gameObjects.
+/// If its scene-specific stuff, it probably shouldn't be this kind of singleton.
+/// 
+/// Specific implementation is from Hands-On Game Development Patterns with Unity 2019 by David Baron.
 /// </summary>
 public class Singleton<T> : MonoBehaviour where T : Component
 {
@@ -20,8 +23,7 @@ public class Singleton<T> : MonoBehaviour where T : Component
             _instance = FindObjectOfType<T>();//check for other instances.
             if (_instance == null)
             {
-               GameObject obj = new GameObject();
-               obj.name = typeof(T).Name;
+               GameObject obj = new GameObject {name = typeof(T).Name};
                _instance = obj.AddComponent<T>();
             }
          }
